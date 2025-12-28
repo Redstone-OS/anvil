17c2f294e8bcee301aa0080368164b25849af109
João Victor P. <joaonhk@gmail.com>
2025-12-28 09:26:02 -0300

    revisto

--- src/core/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Módulo core - Lógica central do Anvil
-
-pub mod builder;
-pub mod config;
-pub mod recipe;
-pub mod template;
-pub mod utils;
-
-// TODO(prioridade=média, versão=v1.0): Implementar módulos core
